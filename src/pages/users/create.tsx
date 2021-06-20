1935a2f1650ce822b7df8b43c0a4e29ae6566afa
Peterson Pereira <petersonbozza7@gmail.com>
2021-06-20 08:37:04 -0300

    adicionada tela de criacao do usuario

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,39 @@
+import { Box, Button, Flex, HStack, Heading, Divider, VStack, SimpleGrid } from '@chakra-ui/react'
+import { Sidebar } from '../../components/Sidebar'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Form/Input'
+
+export default function CreateUser(){
+  return(
+    <Box>
+      <Header/>
+      <Flex  w='100%' my='6' maxWidth={1480} max='auto' px='6'>
+        <Sidebar/>
+
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+          <Heading size='lg' fontWeight='normal'>Criar usuário</Heading>
+
+          <Divider my='6' borderColor='gray.700'/>
+          <VStack spacing='8'>
+            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+              <Input name='name' label='Nome Completo'/>
+              <Input name='email' type='email' label='E-mail'/>
+            </SimpleGrid>
+            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+              <Input name='password' type='password' label='Senha'/>
+              <Input name='confirmation_password' type='password' label='Confirme sua Senha'/>
+            </SimpleGrid>
+          </VStack>
+          <Flex mt='8' justify='flex-end'>
+            <HStack spacing='4'>
+              <Button colorScheme='whiteAlpha'>Cancelar</Button>
+              <Button colorScheme='pink'>Salvar</Button>
+            </HStack>
+
+          </Flex>
+        </Box>
+
+      </Flex>
+    </Box>
+  )
+}
